1f2c73bd13e2e02d9d6a790bc513f45c7361c4db
ofinke <65545889+ofinke@users.noreply.github.com>
2021-04-24 12:58:36 +0200

    2021-04-24
    scraper simplified, started working on daily trading analysis

--- scraper.py
+++ scraper.py
@@ -17,60 +17,37 @@ import datetime as dt
 #import twitterscraper as ts check if this doesnt send some weird shit somewhere
 import yfinance as yf
 
-class scrap():
+def scraptwitter(stockHandle):
+     
+    print("\nScraping https://twitter.com/")
+    print("Status: Failed (scraper not implemented)")
+    
+    # query
 
-    def __init__(self, stockHandle):
-        # constructor calls methods specified by the user 
-        # modyfies data and returns object with:
-        # self.data => pandas dataframe with stock market data
-        # self.info => information about scraped stock
+def scrapstockdata(stockHandle, d, i, ed):
+    # d = days
+    # i = interval
+    # ed = end date
+    # use yfinance
+    print("\nScraping https://finance.yahoo.com/")
+    print("Status: started")
 
-        print("   _____                                ")
-        print("  / ___/______________ _____  ___  _____")
-        print("  \__ \/ ___/ ___/ __ `/ __ \/ _ \/ ___/")
-        print(" ___/ / /__/ /  / /_/ / /_/ /  __/ /    ")
-        print("/____/\___/_/   \__,_/ .___/\___/_/     ")
-        print("                    /_/                 ")
-        print("\nVersion 0.1")
-        print("\nScraping for stock(s) " + stockHandle + " data.")
+    stockTicket = yf.Ticker(stockHandle)
 
+    # scrape relevant data
+    data = stockTicket.history(start=(ed - dt.timedelta(days=d)), end=ed, interval=i, actions=False)
+    data.reset_index(inplace=True)
 
-        self.scrapstockdata(stockHandle)
-        # self.scraptwitter(stockHandle)
-        
-        print("\nScraping completed")
+    info = stockTicket.info
 
-    def scraptwitter(self, stockHandle):
-        
-        print("\nScraping https://twitter.com/")
-        print("Status: Failed (scraper not implemented)")
-        
-        # query
-
-    def scrapstockdata(self, stockHandle):
-        # use yfinance
-        print("\nScraping https://finance.yahoo.com/")
-        print("Status: started")
-
-        stockTicket = yf.Ticker(stockHandle)
-
-        # scrape relevant data
-        print("Status: scraping last 3 weeks")
-        self.data = stockTicket.history(start=(dt.date.today() - dt.timedelta(days=21)), end=dt.date.today(), interval="5m", actions=False)
-        self.data.reset_index(inplace=True)
-
-        self.info = stockTicket.info
-
-
-        print("Status: completed")
-        print("data saved as .dataweeks and .info")
-        return
+    print("Status: completed")
+    return (data, info)
 
 
 # RUNTIME
 # script runtime
 def main():
-    d = scrap(stockHandle="TSLA")
+    
     return
 
 if __name__ == "__main__":
