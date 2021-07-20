6c7e34be44aa0a77e726190701dde64ce1c78aeb
Alexandr Spacek <spacealexandr@gmail.com>
2021-07-20 23:41:11 +0200

    printing results in main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from scraper import stock_daily
 from analyzer import Analyzer
-
-TSLA = Analyzer(ticker='TSLA',data=stock_daily('TSLA').data)
-tradeSummary = TSLA.profit(buyMethodName='Mcstoch_ut1',sellMethodName='Mcstoch',capitalForEachTrade=300,comission=2)
+import numpy as np
+ticker_name = 'HGSH'
+anal = Analyzer(ticker=ticker_name,data=stock_daily(ticker_name).data)
+tradeSummary = anal.profit(buyMethodName='Mcstoch_ut1',sellMethodName='Mcstoch',capitalForEachTrade=400,comission=2)
 profitAbsolute = tradeSummary["profit[$]"].sum()
-print(profitAbsolute)
+profitRelative = tradeSummary["profit[%]"].sum()
+profitByHolding = 100*((tradeSummary["sell_price"].iloc[-2]-tradeSummary["sell_price"].iloc[0])/tradeSummary["sell_price"].iloc[0])  if np.isnan(tradeSummary["sell_price"].iloc[-1]) else 100*((tradeSummary["sell_price"].iloc[-1]-tradeSummary["sell_price"].iloc[0])/tradeSummary["sell_price"].iloc[0])
+print(tradeSummary)
+print('Absolute profit during last year: ',profitAbsolute,'$')
+print('Relative profit during last year: ',profitRelative,'%')
+print('Relative profit by holding during last year: ',profitByHolding,'%')
 
