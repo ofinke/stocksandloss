72645a7a2072332c8ce80084aab40fa2d3ea3a96
ofinke <ondrej.finke@gmail.com>
2021-07-21 14:05:00 +0200

    2021-07-21-B
    Added tester jupyter notebook

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from scraper import stock_daily
 from analyzer import Analyzer
 import numpy as np
-ticker_name = 'TSLA'
-anal = Analyzer(ticker=ticker_name,data=stock_daily(ticker_name).data)
+ticker_name = "AAPL"
+anal = Analyzer(ticker=ticker_name,data=stock_daily(ticker_name, save=False).data)
 
 tradeSummary = anal.profit(buyMethodName='Mcstoch_ut1',sellMethodName='Mcstoch',capitalForEachTrade=400,comission=2)
 profitAbsolute = tradeSummary["profit[$]"].sum()
 profitRelative = tradeSummary["profit[%]"].sum()
 profitByHolding = 100*((anal.data["Close"].iloc[-1]-anal.data["Close"].iloc[0])/anal.data["Close"].iloc[0])
 print(tradeSummary)
-print('Absolute profit during last year: ',profitAbsolute,'$')
-print('Relative profit during last year: ',profitRelative,'%')
-print('Relative profit by holding during last year: ',profitByHolding,'%')
+print('Absolute profit during last year: ',np.round(profitAbsolute,2),'$')
+print('Relative profit during last year: ',np.round(profitRelative,2),'%')
+print('Relative profit by holding during last year: ',np.round(profitByHolding,2),'%')
 
 
